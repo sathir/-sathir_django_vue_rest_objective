d8fcbf21b2dfa13ab52aa244c52a03713e1d3d07
Mansoor M. Sathir <sathir@live.com>
2022-01-01 21:14:51 +0530

    Vaccine model created for record vaccine details

--- adoptions/models.py
+++ adoptions/models.py
@@ -10,3 +10,6 @@ class Pet(models.Model):
     sex = models.CharField(max_length=1,choices=SEX_CHOICES,blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)

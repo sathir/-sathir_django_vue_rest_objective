61f08ace726803ce7f8ce19e3204fd6265505e94
Mansoor M. Sathir <sathir@live.com>
2022-01-16 23:47:28 +0530

    Add html views

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from adoptions.models import Pet
 
 def home(request):
     pets = Pet.objects.all()
-    return HttpResponse('<p>home view</p>')
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
 
 def pet_detail(request, pet_id):
-    return HttpResponse('<p>pet_detail view with id {pet_id} </p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })

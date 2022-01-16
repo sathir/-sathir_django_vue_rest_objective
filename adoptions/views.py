6bd194b595a80f03ba254dce7d78a9efb4d4f54e
Mansoor M. Sathir <sathir@live.com>
2022-01-16 23:46:35 +0530

    Urls created with views

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from adoptions.models import Pet
+
+def home(request):
+    pets = Pet.objects.all()
+    return HttpResponse('<p>home view</p>')
+
+def pet_detail(request, pet_id):
+    return HttpResponse('<p>pet_detail view with id {pet_id} </p>')

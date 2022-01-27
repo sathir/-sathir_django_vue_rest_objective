04b1c4fe674ec8b4860cb41dbacb50bdcc87636d
Mansoor M. Sathir <sathir@live.com>
2022-01-27 23:19:31 +0530

    Pagination set

--- adoptions/api_views.py
+++ adoptions/api_views.py
@@ -1,7 +1,14 @@
-from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
+
 from adoptions.models import Pet
 from adoptions.serializers import PetSerializer
+
+class PetPagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 100
 
 class PetList(ListAPIView):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+    pagination_class = PetPagination
